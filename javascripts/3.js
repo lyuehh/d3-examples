2c8a7f4ea341cdf61c07c251923911b3ce8054d1
lyuehh <lyuehh@gmail.com>
2013-08-31 17:24:44 +0800

    change colors

--- javascripts/3.js
+++ javascripts/3.js
@@ -2,6 +2,8 @@
 var w = 500;
 var h = 500;
 
+var color = d3.scale.category20();
+
 // svg
 var svg = d3.select('#container')
   .append('svg')
@@ -13,13 +15,13 @@ var c = svg.append('circle')
   .attr('cx', w/2)
   .attr('cy', w/2)
   .attr('r', w/2)
-  .attr('fill', 'green');
+  .attr('fill', color(0));
 
 var c2 = svg.append('circle')
     .attr('cx', w/2)
     .attr('cy', w/2)
     .attr('r', 150)
-    .attr('fill', 'red');
+    .attr('fill', color(1));
 
 // rect
 svg.append('rect')
@@ -28,28 +30,28 @@ svg.append('rect')
     .attr('width', 150)
     .attr('height', 150)
     .attr('transform', "rotate(45, 250, 250)")
-    .attr('fill', 'darkblue');
+    .attr('fill', color(4));
 
 svg.append('circle')
     .attr('cx', 50)
     .attr('cy', 250)
     .attr('r', 50)
-    .attr('fill', 'darkblue');
+    .attr('fill', color(2));
 
 svg.append('circle')
     .attr('cx', 450)
     .attr('cy', 250)
     .attr('r', 50)
-    .attr('fill', 'darkblue');
+    .attr('fill', color(2));
 
 svg.append('circle')
     .attr('cx', 250)
     .attr('cy', 50)
     .attr('r', 50)
-    .attr('fill', 'darkblue');
+    .attr('fill', color(2));
 
 svg.append('circle')
     .attr('cx', 250)
     .attr('cy', 450)
     .attr('r', 50)
-    .attr('fill', 'darkblue');
+    .attr('fill', color(2));
